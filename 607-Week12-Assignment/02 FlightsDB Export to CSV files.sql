3171d7812b9384f56d3ee586a1e58708f9d1a457
ChadRyanBailey <cbailey@users.noreply.github.com>
2018-11-25 15:20:26 -0500

    initial uploads

---
+++ 607-Week12-Assignment/02 FlightsDB Export to CSV files.sql
@@ -0,0 +1,81 @@
+
+
+USE flights;
+
+
+-- get the folder where files must be uploaded/downloaded under MySQLs new security settings
+	-- SHOW VARIABLES LIKE "secure_file_priv";	
+
+-- export tables
+	
+    -- flights.airlines    
+		SELECT 'Carrier', 'Name'
+		UNION SELECT * 
+			INTO OUTFILE 
+			'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/Data 607/Flights Export/airlines.csv'
+				FIELDS TERMINATED BY ',' 
+				OPTIONALLY ENCLOSED BY '"'
+				ESCAPED BY '\\'
+				LINES TERMINATED BY '\n'
+			FROM airlines;
+            
+	-- flights.airports
+		SELECT 'FAA', 'Name', 'Lat', 'Lon', 'Alt', 'TZ', 'Dst'
+		UNION SELECT * 
+			INTO OUTFILE 
+			'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/Data 607/Flights Export/airports.csv'
+				FIELDS TERMINATED BY ',' 
+				OPTIONALLY ENCLOSED BY '"'
+				ESCAPED BY '\\'
+				LINES TERMINATED BY '\n'
+			FROM airports;
+            
+	-- flights.flights
+		SELECT 'Year', 'Month', 'Day', 'Dep_Time', 'Dep_Delay', 'arr_Time', 'arr_Delay', 'Carrier', 
+				'TailNum', 'Flight', 'Origin', 'Dest', 'Air_Time', 'Distance', 'Hour', 'Minute'
+		UNION SELECT * 
+			INTO OUTFILE 
+			'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/Data 607/Flights Export/flights.csv'
+				FIELDS TERMINATED BY ',' 
+				OPTIONALLY ENCLOSED BY '"'
+				ESCAPED BY '\\'
+				LINES TERMINATED BY '\n'
+			FROM flights;
+            
+	-- flights.planes
+		SELECT 'TailNum', 'Year', 'Type', 'Manufacturer', 'Model', 'Engine', 'Seats', 'Speed', 'Engine'
+		UNION SELECT * 
+			INTO OUTFILE 
+			'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/Data 607/Flights Export/planes.csv'
+				FIELDS TERMINATED BY ',' 
+				OPTIONALLY ENCLOSED BY '"'
+				ESCAPED BY '\\'
+				LINES TERMINATED BY '\n'
+			FROM planes;
+            
+	-- flights.weather
+		SELECT 'Origin', 'Year', 'Month', 'Day', 'Hour', 'Temp', 'DewP', 'Humid', 
+				'Wind_Dir', 'Wind_Speed', 'Wind_Gust', 'Precip', 'Pressure', 'Visib'
+		UNION SELECT * 
+			INTO OUTFILE 
+			'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/Data 607/Flights Export/weather.csv'
+				FIELDS TERMINATED BY ',' 
+				OPTIONALLY ENCLOSED BY '"'
+				ESCAPED BY '\\'
+				LINES TERMINATED BY '\n'
+			FROM weather;
+			
+
+
+    
+    
+
+    
+    
+    
+
+
+
+
+
+
